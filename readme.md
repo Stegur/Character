1de9e14f067690cd2aa5e67b426376188cb2e0a8
Stegura Alex <stegura.alex@gmail.com>
2019-12-09 13:23:49 +0300

    done

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Character
+
+[![Build status](https://ci.appveyor.com/api/projects/status/b4478c7kdfnjfr41/branch/master?svg=true)](https://ci.appveyor.com/project/Stegur/character/branch/master)
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
-// TODO: write your code here
-import sum from './basic';
+import Character from './Character';
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+const hero = new Character('Tristan', 'Bowman');
+console.log(hero);
